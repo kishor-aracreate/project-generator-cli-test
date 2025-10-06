f6fdcac88ad473720d2260e9b8268ff099d182ca
Kishor Arjunan <kishor@aracreate.group>
2025-10-06 10:45:26 +0530

    feat: add the scaffold with react template (#1)

---
+++ bin/index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { createApp } from "../src/scaffold.js";
+
+async function main() {
+  console.log(chalk.green("🚀 Welcome to My CLI Tool"));
+
+  const args = process.argv.slice(2);
+  let projectName = args[0];
+
+  const questions = [];
+
+  if (!projectName) {
+    questions.push({
+      type: "input",
+      name: "projectName",
+      message: "Project name:",
+      default: "my-app"
+    });
+  }
+
+  questions.push({
+    type: "list",
+    name: "framework",
+    message: "Choose a framework:",
+    choices: ["React", "Angular", "Vanilla JS"]
+  });
+
+  const answers = await inquirer.prompt(questions);
+
+  // if projectName was passed as arg, prefer it
+  const finalAnswers = {
+    ...answers,
+    projectName: projectName || answers.projectName
+  };
+
+  await createApp(finalAnswers);
+}
+
+main();
